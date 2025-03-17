2b65b7b24f827046d4debdee10d6cbe063844689
Andrés Gutiérrez <psirusteam@Andress-Mac-mini.local>
2025-03-17 15:22:23 -0300

    3.3 done

--- rcodes/3.3 Correlations.R
+++ rcodes/3.3 Correlations.R
@@ -1,10 +1,10 @@
 ################################################################################
 # 3.3 Correlations
 ################################################################################
-# Author: Andrés Gutiérrez, Stalyn Guerrero
-# 
+# Author: Andrés Gutiérrez & Stalyn Guerrero
+#
 # Data Source:
-# https://microdata.worldbank.org/index.php/catalog/3823/data-dictionary 
+# https://microdata.worldbank.org/index.php/catalog/3823/data-dictionary
 # Version:  1
 # Date    :  13-03-2025
 ################################################################################
@@ -38,50 +38,48 @@ select <- dplyr::select
 #                           Loading Datasets                                   #
 #------------------------------------------------------------------------------#
 
-dat_pers <- read_sav("data/data_ESS4/cons_agg_w4.sav") %>% 
+IND_data <- read_sav("data/data_ESS4/cons_agg_w4.sav") %>%
   mutate(strata = paste0(saq01, "_", saq14))
 
 #------------------------------------------------------------------------------#
 #                       Defining Survey Design                                #
 #------------------------------------------------------------------------------#
-options(survey.lonely.psu = "fail") 
-
-design_sampling <- dat_pers %>% 
+ESS4_design <- IND_data %>%
   as_survey_design(
-    ids = ea_id,  # Primary sampling unit identifier (EA)
-    strata = strata, # Stratification by region (saq01) and urban/rural zone (saq14)
-    weights = pw_w4,  # Final adjusted weight
+    ids = ea_id,
+    # Primary sampling unit identifier (EA)
+    strata = strata,
+    # Stratification by region (saq01) and urban/rural zone (saq14)
+    weights = pw_w4,
+    # Final adjusted weight
     nest = TRUE
   )
 
+options(survey.lonely.psu = "fail")
+summary(ESS4_design)
+
 #------------------------------------------------------------------------------#
-#                   Computing Correlations                                    #
+#                   Estimating Correlations                                    #
 #------------------------------------------------------------------------------#
 
 # Overall correlation
-design_sampling %>%
-  summarise(
-    corr1 = survey_corr(educ_cons_ann, utilities_cons_ann), 
-    corr2 = survey_corr(educ_cons_ann, fafh_cons_ann)
-  ) %>% 
-  kable(digits = 2)
+ESS4_design %>% summarise(
+  corr1 = survey_corr(educ_cons_ann, utilities_cons_ann),
+  corr2 = survey_corr(educ_cons_ann, fafh_cons_ann)
+) 
 
 # Correlation grouped by urban/rural area
-design_sampling %>% 
+ESS4_design %>%
   group_by(area = as_factor(saq14)) %>%
   summarise(
-    corr1 = survey_corr(educ_cons_ann, utilities_cons_ann), 
+    corr1 = survey_corr(educ_cons_ann, utilities_cons_ann),
     corr2 = survey_corr(educ_cons_ann, fafh_cons_ann)
-  ) %>% 
-  kable(digits = 2)
+  ) 
 
 # Correlation grouped by region
-design_sampling %>% 
+ESS4_design %>% 
   group_by(Region = as_factor(saq01)) %>%
   summarise(
     corr1 = survey_corr(educ_cons_ann, utilities_cons_ann), 
     corr2 = survey_corr(educ_cons_ann, fafh_cons_ann)
-  ) %>% 
-  kable(digits = 2)
-
-
+  )
